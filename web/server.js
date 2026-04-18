194b6380882c2188b00fd10af99d4a1d5361d7ac
meninol2023-cell <meninol2023@gmail.com>
2026-04-18 12:16:31 -0300

    Create server.js

---
+++ web/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot online!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("🌐 Web server rodando"));
